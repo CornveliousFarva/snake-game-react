3577d69541559836568d5678743499552ff091c9
Greg Schuman <gcschuman@hotmail.com>
2024-01-22 12:45:11 -0800

    Added components and added the script for the snake.

---
+++ src/Components/Snake.js
@@ -0,0 +1,18 @@
+// Note to self: do app.js LAST
+import React from "react";
+
+const Snake = (props) => {
+    return(
+        <div>
+            {props.snakeDots.map((dot, i) => {
+                const style = {
+                    left: `${dot[0]} %`,
+                    top: `${dot[1]} %`
+                };
+                return <div className="snake" key={i} style={style} />
+            })};
+        </div>
+    );
+};
+
+export default Snake;
