5da8d481bc822019ffe66fdaa8e633e0df79088d
Greg Schuman <gcschuman@hotmail.com>
2024-02-01 12:05:30 -0800

    Added on keydown commands.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// Imports
+import React, { Component } from "react";
+import Snake from "./Components/Snake";
+import Food from "./Components/Food";
+import Button from "./Components/Button";
+import Menu from "./Components/Menu";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Variable that generates food in random places
+const getRandomFood = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  return [x, y];
 }
 
-export default App;
+// Initial State Variable
+const initialState = {
+  food: getRandomFood(),
+  direction: "RIGHT",
+  speed: 100,
+  route: "menu",
+  snakeDots: [
+    [0, 0],
+    [0, 2]
+  ],
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  componentDidMount(){
+    setInterval(this.moveSnake, this.state.speed);
+    document.onkeydown = this.onKeyDown;
+  }
+
+  componentDidUpdate(){
+    this.onSnakeOutOfBounds();
+    this.onSnakeCollapsed();
+    this.onSnakeEats();
+  }
+
+  onKeyDown = (e) => { 
+    e.preventDefault(); 
+    e = e || window.event; 
+    switch (e.keyCode) { 
+        case 37: 
+            this.setState({ direction: "LEFT" }); 
+            break; 
+        case 38: 
+            this.setState({ direction: "UP" }); 
+            break; 
+        case 39: 
+            this.setState({ direction: "RIGHT" }); 
+            break; 
+        case 40: 
+            this.setState({ direction: "DOWN" }); 
+            break; 
+    } 
+}
